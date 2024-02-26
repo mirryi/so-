d6e9d27824c2080c15e9b9a56f2f3867d5422b96
Eric Zhao <21zhaoe@protonmail.com>
2024-02-26 02:14:16 -0500

    ts: fix execution fragment definition

--- TS/Basic.lean
+++ TS/Basic.lean
@@ -1,5 +1,4 @@
 import Mathlib.Data.Set.Defs
-import Mathlib.Data.Set.Card
 import Mathlib.Data.Set.Function
 
 variable {s a p : Type}
@@ -14,16 +13,16 @@ variable (ts : @TS s a p)
 def TS.postOn (s : s) (τ : a) :=
   ts.trans (s, τ)
 def TS.post (s : s) :=
-  { s' | ∃ τ : a, s' ∈ TS.postOn ts s τ }
+  { s' | ∃ τ : a, s' ∈ ts.postOn s τ }
 def TS.setPost (c : Set s) :=
-  { s' | ∃ s ∈ c, s' ∈ TS.post ts s }
+  { s' | ∃ s ∈ c, s' ∈ ts.post s }
 
 def TS.preOn (s : s) (τ : a) :=
-  { s' | s ∈ TS.postOn ts s' τ }
+  { s' | s ∈ ts.postOn s' τ }
 def TS.pre (s : s) :=
-  { s' | ∃ τ : a, s' ∈ TS.preOn ts s τ }
+  { s' | ∃ τ : a, s' ∈ ts.preOn s τ }
 def TS.setPre (c : Set s) :=
-  { s' | ∃ s ∈ c, s' ∈ TS.pre ts s }
+  { s' | ∃ s ∈ c, s' ∈ ts.pre s }
 
 def TS.Terminal (s : s) :=
-  TS.post ts s = ∅
+  ts.post s = ∅
--- TS/Execution.lean
+++ TS/Execution.lean
@@ -1,26 +1,30 @@
+import Mathlib.Data.Vector.Basic
+
 import TS.Basic
 
-variable {s a p : Type}
+variable {s a p : Type} (ts : @TS s a p) (n : ℕ)
 
-structure ExecutionFrag where
-  start : s
-  sequence : List (a × s)
+structure TS.ExecutionFragment where
+  states  : Vector s n.succ
+  actions : Vector a n
+  valid   : ∀ i : Fin n, states.get i.succ ∈ ts.trans (states.get i.castSucc, actions.get i)
 
-variable (ts : @TS s a p)
-         (ρ  : @ExecutionFrag s a)
+variable (ρ : ts.ExecutionFragment n)
 
-def ExecutionFrag.end :=
-  List.getLast (ρ.start :: (List.map Prod.snd ρ.sequence)) (by simp)
+def TS.ExecutionFragment.start :=
+  ρ.states.head
+def TS.ExecutionFragment.end :=
+  ρ.states.last
 
-def ExecutionFrag.Initial :=
+def TS.ExecutionFragment.Initial :=
   ρ.start ∈ ts.initial
-def ExecutionFrag.Maximal :=
-  TS.Terminal ts (ExecutionFrag.end ρ)
+def TS.ExecutionFragment.Maximal :=
+  ts.Terminal ρ.end
 
-def Execution :=
-  { ρ : ExecutionFrag // ExecutionFrag.Initial ts ρ ∧ ExecutionFrag.Maximal ts ρ }
+def TS.Execution :=
+  { ρ : ts.ExecutionFragment n // ρ.Initial ∧ ρ.Maximal }
 
+def TS.ReachableN :=
+  { st : s // ∃ ρ : ts.Execution n, ρ.1.end = st }
 def TS.Reachable :=
-  { s : s // ∃ ρ : Execution ts, ExecutionFrag.end ρ.1 = s }
-def TS.Reach :=
-  { s | s : TS.Reachable ts }
+  { st : s // ∀ n, ∃ ρ : ts.Execution n, ρ.1.end = st }
