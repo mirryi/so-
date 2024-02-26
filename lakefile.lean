a25b7d04eb3ac404052ce8dd1c08bee4a4ecb7af
Eric Zhao <21zhaoe@protonmail.com>
2024-02-25 19:51:33 -0500

    ts: WIP define transition system

--- lakefile.lean
+++ lakefile.lean
@@ -3,9 +3,12 @@ open Lake DSL
 
 package «soþ» where
 
+lean_lib «TS»  where
 lean_lib «CTL» where
 lean_lib «Soþ» where
 
 @[default_target]
 lean_exe «soþ» where
   root := `Main
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
