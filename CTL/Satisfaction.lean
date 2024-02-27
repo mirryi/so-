439480433aa3989b1700d339cce2eed000b0bd42
Eric Zhao <21zhaoe@protonmail.com>
2024-02-26 19:24:16 -0500

    ctl: prove that ts satisfies formula iff initial states included in satisfying states

--- CTL/Satisfaction.lean
+++ CTL/Satisfaction.lean
@@ -10,6 +10,7 @@ variable {s a p : Type}
          (φ φ₁ φ₂ : @PathFormula p)
 
 mutual
+  -- s ⊨ Φ
   def TS.StateSatisfaction (ts : @TS s a p) (st : s) :=
     fun
       | StateFormula.top => True
@@ -19,28 +20,30 @@ mutual
       | ⬝∃ φ             => ∃ π : ts.Path, ts.PathSatisfaction π φ
       | ⬝∀ φ             => ∀ π : ts.Path, ts.PathSatisfaction π φ
 
+  -- π ⊨ φ
   def TS.PathSatisfaction (ts : @TS s a p) (π : ts.Path) :=
     fun
-     | ⬝◯ Φ => ts.StateSatisfaction (π.1.2.get 1) Φ
+     | ⬝◯ Φ   => ts.StateSatisfaction (π.1.2.get 1) Φ
      | Φ ⬝U Ψ => ∃ j, (∀ k < j, ts.StateSatisfaction (π.1.2.get k) Φ) ∧ (ts.StateSatisfaction (π.1.2.get j) Ψ)
 end
 
+-- Sat(Φ)
 def TS.Sat := { st : s // ts.StateSatisfaction st Φ }
-def TS.satSet := Subtype.val '' (Set.univ : Set (ts.Sat Φ))
+def TS.satSet := @Set.range s (ts.Sat Φ) Subtype.val
 
+-- TS ⊨ Φ
 def TS.Satisfaction := ∀ st ∈ ts.initial, ts.StateSatisfaction st Φ
 
-theorem TS.Satisfaction_subset : (ts.Satisfaction Φ) ↔ (ts.initial ⊆ ts.satSet Φ) :=
+@[simp]
+theorem TS.sat_iff : (ts.Satisfaction Φ) ↔ (ts.initial ⊆ ts.satSet Φ) :=
   by
     apply Iff.intro
     . intro sat st mem
-      -- exact sat st mem
-      constructor
-      . intros
-        done
-    . intro sub x y
-      rw [Set.subset_def] at sub
-      specialize sub x
-      specialize sub y
-      --
-    done
+      exact Set.mem_range_self (⟨st, sat st mem⟩ : ts.Sat Φ)
+    . intro sub st mem
+      specialize sub mem
+      unfold TS.satSet at sub
+      rw [Set.mem_range] at sub
+      obtain ⟨stSat, refl⟩ := sub
+      rw [← refl]
+      exact Subtype.property stSat
