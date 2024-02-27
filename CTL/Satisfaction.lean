999c5426911a7763be71250c9a9d4902e58948e1
Eric Zhao <21zhaoe@protonmail.com>
2024-02-26 22:16:27 -0500

    ctl: clean up satisfaction item names

--- CTL/Satisfaction.lean
+++ CTL/Satisfaction.lean
@@ -11,47 +11,47 @@ variable {s a p : Type}
 
 mutual
   -- s ⊨ Φ
-  def TS.StateSatisfaction (ts : @TS s a p) :=
+  def TS.StateSat (ts : @TS s a p) :=
     fun Φ st => match Φ with
       | StateFormula.top => True
       | ⬝a               => a ∈ ts.label st
-      | ⬝¬Φ              => ¬(ts.StateSatisfaction Φ st)
-      | Φ₁ ⬝∧ Φ₂         => (ts.StateSatisfaction Φ₁ st) ∧ (ts.StateSatisfaction Φ₂ st)
-      | ⬝∃ φ             => ∃ π : ts.PathFrom st, ts.PathSatisfaction π.1.2 φ
-      | ⬝∀ φ             => ∀ π : ts.PathFrom st, ts.PathSatisfaction π.1.2 φ
+      | ⬝¬Φ              => ¬(ts.StateSat Φ st)
+      | Φ₁ ⬝∧ Φ₂         => (ts.StateSat Φ₁ st) ∧ (ts.StateSat Φ₂ st)
+      | ⬝∃ φ             => ∃ π : ts.PathFrom st, ts.PathSat π.1.2 φ
+      | ⬝∀ φ             => ∀ π : ts.PathFrom st, ts.PathSat π.1.2 φ
 
   -- π ⊨ φ
-  def TS.PathSatisfaction (ts : @TS s a p) (π : ts.PathFragment n) :=
+  def TS.PathSat (ts : @TS s a p) (π : ts.PathFragment n) :=
     fun
-     | ⬝◯ Φ   => ts.StateSatisfaction Φ (π.1.get 1)
-     | Φ ⬝U Ψ => ∃ j, (∀ k < j, ts.StateSatisfaction Φ (π.get k)) ∧ (ts.StateSatisfaction Ψ (π.get j))
+     | ⬝◯ Φ   => ts.StateSat Φ (π.1.get 1)
+     | Φ ⬝U Ψ => ∃ j, (∀ k < j, ts.StateSat Φ (π.get k)) ∧ (ts.StateSat Ψ (π.get j))
 end
 
 -- Sat(Φ)
-def TS.Sat := { st : s // ts.StateSatisfaction Φ st }
-def TS.satSet := @Set.range s (ts.Sat Φ) Subtype.val
+def TS.SatState := { st : s // ts.StateSat Φ st }
+def TS.satStateSet := @Set.range s (ts.SatState Φ) Subtype.val
 
 -- TS ⊨ Φ
-def TS.Satisfaction := ∀ st ∈ ts.initial, ts.StateSatisfaction Φ st
+def TS.Sat := ∀ st ∈ ts.initial, ts.StateSat Φ st
 
 @[simp]
-theorem TS.sat_iff : (ts.Satisfaction Φ) ↔ (ts.initial ⊆ ts.satSet Φ) :=
+theorem TS.Sat.subset_iff : (ts.Sat Φ) ↔ (ts.initial ⊆ ts.satStateSet Φ) :=
   by
     apply Iff.intro
     . intro sat st mem
-      exact Set.mem_range_self (⟨st, sat st mem⟩ : ts.Sat Φ)
+      exact Set.mem_range_self (⟨st, sat st mem⟩ : ts.SatState Φ)
     . intro sub st mem
       specialize sub mem
-      unfold TS.satSet at sub
+      unfold TS.satStateSet at sub
       rw [Set.mem_range] at sub
       obtain ⟨stSat, refl⟩ := sub
       rw [← refl]
       exact Subtype.property stSat
 
 @[simp]
-theorem TS.potentialAll_sat : ts.StateSatisfaction (⬝∃■Φ) st ↔ ∃π : ts.PathFrom st, ∀j, ts.StateSatisfaction Φ (π.1.2.get j) := by
-  simp [StateFormula.potentialAll, TS.StateSatisfaction, StateFormula.inevitable, TS.PathSatisfaction]
+theorem TS.StateSat.potentialAll_def : ts.StateSat (⬝∃■Φ) st ↔ ∃π : ts.PathFrom st, ∀j, ts.StateSat Φ (π.1.2.get j) := by
+  simp [StateFormula.potentialAll, TS.StateSat, StateFormula.inevitable, TS.PathSat]
 
 @[simp]
-theorem TS.invariant_sat : ts.StateSatisfaction (⬝∀■Φ) st ↔ ∀π : ts.PathFrom st, ∀j, ts.StateSatisfaction Φ (π.1.2.get j) := by
-  simp [StateFormula.invariant, TS.StateSatisfaction, StateFormula.potential, TS.PathSatisfaction]
+theorem TS.StateSat.invariant_def : ts.StateSat (⬝∀■Φ) st ↔ ∀π : ts.PathFrom st, ∀j, ts.StateSat Φ (π.1.2.get j) := by
+  simp [StateFormula.invariant, TS.StateSat, StateFormula.potential, TS.PathSat]
