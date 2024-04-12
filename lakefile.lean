714ba59d5ff1cd63add2801e5a574c3b04f1b544
Eric Zhao <21zhaoe@protonmail.com>
2024-04-12 12:59:40 -0400

    chore: add doc-gen4 and update dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -12,3 +12,6 @@ lean_exe «soþ» where
   root := `Main
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
