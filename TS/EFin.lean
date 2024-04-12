dfef2b75eac114f2ecb92e98a38bab320482a395
Eric Zhao <21zhaoe@protonmail.com>
2024-04-12 15:02:14 -0400

    ts: format EFin.lean

--- TS/EFin.lean
+++ TS/EFin.lean
@@ -5,71 +5,70 @@ inductive EFin : ℕ∞ → Type where
   | inf (i : ℕ) : EFin ⊤
 
 namespace EFin
-@[inline]
-def mk {n : ℕ∞} (i : ℕ) (lt : i < n) : EFin n :=
-  match n with
-  | ⊤      => .inf i
-  | some _ => .fin i (WithTop.some_lt_some.1 lt)
+  @[inline]
+  def mk {n : ℕ∞} (i : ℕ) (lt : i < n) : EFin n :=
+    match n with
+    | some _ => .fin i (WithTop.some_lt_some.1 lt)
+    | ⊤      => .inf i
 
-@[inline]
-def val : EFin n → ℕ
-  | .fin i _ => i
-  | .inf i   => i
+  @[inline]
+  def val : EFin n → ℕ
+    | .fin i _ => i
+    | .inf i   => i
 
-attribute [coe] EFin.val
+  attribute [coe] EFin.val
 
-@[inline]
-def lt (i : EFin n) : i.val < n :=
-  match i with
-  | .fin _ lt => WithTop.coe_lt_coe.2 lt
-  | .inf i    => WithTop.coe_lt_top i
+  @[inline]
+  def lt (i : EFin n) : i.val < n :=
+    match i with
+    | .fin _ lt => WithTop.coe_lt_coe.2 lt
+    | .inf i    => WithTop.coe_lt_top i
 
-@[inline]
-def succ : EFin n → EFin (Order.succ n)
-  | .fin i lt => fin i.succ (Nat.succ_lt_succ lt)
-  | .inf i    => inf i.succ
+  @[inline]
+  def succ : EFin n → EFin (Order.succ n)
+    | .fin i lt => fin i.succ (Nat.succ_lt_succ lt)
+    | .inf i    => inf i.succ
 
-@[inline]
-def castInfty {n : ℕ} (i : EFin n) : EFin ⊤ :=
-  match i with | .fin i _ => .inf i
+  @[inline]
+  def castInfty {n : ℕ} (i : EFin n) : EFin ⊤ :=
+    match i with | .fin i _ => .inf i
 
-@[inline]
-def castLT {m : ℕ} {n : ℕ∞} (h : m < n) (i : EFin m) : EFin n :=
-  match n with
-  | ⊤      => i.castInfty
-  | some _ => match i with | .fin i lt => fin i (Nat.lt_trans lt (WithTop.some_lt_some.1 h))
+  @[inline]
+  def castLT {m : ℕ} {n : ℕ∞} (h : m < n) (i : EFin m) : EFin n :=
+    match n with
+    | some _ => match i with | .fin i lt => fin i (Nat.lt_trans lt (WithTop.some_lt_some.1 h))
+    | ⊤      => i.castInfty
 
-@[inline]
-def castLT' {m n : ℕ} (i : EFin m) (h : i.val < n) : EFin n :=
-  match i with | .fin i _ => (fin i h : EFin n)
+  @[inline]
+  def castLT' {m n : ℕ} (i : EFin m) (h : i.val < n) : EFin n :=
+    match i with | .fin i _ => (fin i h : EFin n)
 
-@[inline]
-def castSucc (i : EFin n) : EFin (Order.succ n) :=
-  match i with
-  | .fin i lt => fin i (Nat.lt_succ_of_lt lt)
-  | .inf i    => inf i
+  @[inline]
+  def castSucc (i : EFin n) : EFin (Order.succ n) :=
+    match i with
+    | .fin i lt => fin i (Nat.lt_succ_of_lt lt)
+    | .inf i    => inf i
 
-@[inline]
-def zero : EFin (Order.succ n) :=
-  match n with
-  | ⊤      => .inf 0
-  | some _ => .fin 0 (Nat.zero_lt_succ _)
+  @[inline]
+  def zero : EFin (Order.succ n) :=
+    match n with
+    | some _ => .fin 0 (Nat.zero_lt_succ _)
+    | ⊤      => .inf 0
 
-@[inline]
-def zero' (h : 0 < n) : EFin n :=
-  match n with
-  | ⊤      => .inf 0
-  | some _ => .fin 0 (WithTop.some_lt_some.1 h)
+  @[inline]
+  def zero' (h : 0 < n) : EFin n :=
+    match n with
+    | some _ => .fin 0 (WithTop.some_lt_some.1 h)
+    | ⊤      => .inf 0
 
-instance : LT (EFin n) where
-  lt i j := LT.lt i.val j.val
+  instance : LT (EFin n) where
+    lt i j := LT.lt i.val j.val
 
-instance : LE (EFin n) where
-  le i j := LE.le i.val j.val
+  instance : LE (EFin n) where
+    le i j := LE.le i.val j.val
 
-@[inline]
-def toFin {n : ℕ} (i : EFin (↑n)) : Fin n := by
-  cases i with
-  | fin i lt => exact ⟨i, lt⟩
-
+  @[inline]
+  def toFin {n : ℕ} (i : EFin (↑n)) : Fin n := by
+    cases i with
+    | fin i lt => exact ⟨i, lt⟩
 end EFin
